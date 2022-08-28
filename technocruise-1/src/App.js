7d4a691b65847f7266bc11276fdd270a38a181e8
its-adityajohri <94777305+its-adityajohri@users.noreply.github.com>
2022-08-28 13:14:55 +0530

    Add files via upload

---
+++ technocruise-1/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import ContactUs from "./MyComponents/Sponsors.js";
+import Navbar from "./MyComponents/Navbar.js";
+
+
+
+function App() {
+  return (
+    <>
+      <ContactUs/>
+      <Navbar/>
+      
+    </>
+    );
+}
+
+export default App;
